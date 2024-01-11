4fc3139433041d414ffae7aea198e6ae2cd2c545
Aaron Styles <aaron@charcoalstyles.com>
2024-01-11 15:22:58 +1100

    A different approach to the testing

--- __tests__/e2e/test.spec.ts
+++ __tests__/e2e/test.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Opens correct NextJS docs', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
   const page1Promise = page.waitForEvent('popup');
   await page.getByRole('link', { name: 'Docs -> Find in-depth' }).click();
   const page1 = await page1Promise;
