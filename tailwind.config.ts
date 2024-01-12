0a58f2a3dfa07ed4f3edce5b4c06c1afc821f807
Aaron Styles <aaron@charcoalstyles.com>
2024-01-12 13:53:53 +1100

    Added basic style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'text': 'var(--text)',
+      'background': 'var(--background)',
+      'primary': 'var(--primary)',
+      'secondary': 'var(--secondary)',
+      'accent': 'var(--accent)',
+     },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
