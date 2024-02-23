0e4a8b6075c0757293e87ee4b4318e6333bca5d2
Aaron Styles <aaron@charcoalstyles.com>
2024-02-23 14:24:27 +1100

    Moved to react-modal, added delete project

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,30 +1,41 @@
 import { useState, useEffect, PropsWithChildren } from "react";
-import PureModal from "react-pure-modal";
+// import PureModal from "react-pure-modal";
+import ReactModal from "react-modal";
 import { Text } from "@/components/ui";
+import clsx from "clsx";
 
 type SbAuthProps = {
-  isOpen?: boolean;
-  onClose?: () => void;
+  isOpen: boolean;
+  onClose: () => void;
   children: JSX.Element;
+  className?: string | ReactModal.Classes;
+  overLayClassName?: string | ReactModal.Classes;
 };
 
-export function Modal({ isOpen, onClose, children }: SbAuthProps) {
+export function Modal({
+  isOpen,
+  onClose,
+  children,
+  className,
+  overLayClassName,
+}: SbAuthProps) {
   return (
-    <>
-      <div
-        className="opacity-40 fixed inset-0 bg-black"
-        onClick={() => {
-          onClose && onClose();
-        }}
-      />
-      <PureModal
-        isOpen={isOpen}
-        onClose={() => {
-          onClose && onClose();
-        }}
-        className="mx-auto mt-32 px-12 py-8 w-2/5 max-w-xl absolute inset-x-0 bg-black">
-        {children}
-      </PureModal>
-    </>
+    <ReactModal
+      ariaHideApp={false}
+      isOpen={isOpen}
+      shouldCloseOnOverlayClick
+      onRequestClose={() => {
+        onClose && onClose();
+      }}
+      overlayClassName={clsx(
+        "fixed inset-0 bg-black bg-opacity-50",
+        overLayClassName
+      )}
+      className={clsx(
+        "mx-auto mt-32 px-12 py-8 w-2/5 max-w-xl absolute inset-x-0 rounded-xl bg-black border border-slate-600",
+        className
+      )}>
+      {children}
+    </ReactModal>
   );
 }
--- src/pages/app/projects/[projectId]/index.tsx
+++ src/pages/app/projects/[projectId]/index.tsx
@@ -1,13 +1,17 @@
 import { AppLayout } from "@/components/AppLayout";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { Button, Text } from "@/components/ui";
+import { Button, Modal, Text } from "@/components/ui";
 import { useProject } from "@/hooks/useProject";
+import { useAtom } from "jotai";
+import { supabaseAtom } from "@/util/supabase";
 
 export default function ProjectDetailsPage() {
   const router = useRouter();
   const project = useProject(router.query.projectId as string);
   const [isLoading, setIsLoading] = useState(true);
+
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
 
   useEffect(() => {
     if (project && !project.isLoading) {
@@ -29,11 +33,32 @@ export default function ProjectDetailsPage() {
         <Text>Loading...</Text>
       ) : (
         <>
+          <Modal
+            isOpen={isDeleteOpen}
+            onClose={() => {
+              setIsDeleteOpen(false);
+            }}>
+            <DeleteModalBody onClose={()=> {
+              setIsDeleteOpen(false);
+            }} projectId={project.data!.id} />
+          </Modal>
           <div className="flex mb-4 flex-row gap-2">
-            <Button size="small" label="✏️" variant="basic" onClick={() => {
-              router.push(`/app/projects/${router.query.projectId}/edit`);
-            }}/>
-            <Button size="small" label="🗑️" variant="black" />
+            <Button
+              size="small"
+              label="✏️"
+              variant="basic"
+              onClick={() => {
+                router.push(`/app/projects/${router.query.projectId}/edit`);
+              }}
+            />
+            <Button
+              size="small"
+              label="🗑️"
+              variant="black"
+              onClick={() => {
+                setIsDeleteOpen(true);
+              }}
+            />
           </div>
           <div className="flex flex-row flex-wrap">
             <div>
@@ -45,3 +70,47 @@ export default function ProjectDetailsPage() {
     </AppLayout>
   );
 }
+
+type DeleteModalBodyProps = {
+  projectId: number;
+  onClose: () => void;
+};
+
+const DeleteModalBody = ({ onClose, projectId }: DeleteModalBodyProps) => {
+  const router = useRouter();
+  const [supabase] = useAtom(supabaseAtom);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  return (
+    <>
+      <Text>Are you sure you want to delete this project?</Text>
+      <div className="flex flex-row gap-2 mt-4">
+        <Button
+          disabled={isDeleting}
+          label="Yes"
+          variant="primary"
+          onClick={() => {
+            setIsDeleting(true);
+            supabase.from("projects").delete().eq("id", projectId).then(({status, error}) => {
+              if (error) {
+                console.error(error);
+              }
+
+              if (status === 204) {
+                router.push("/app/projects");
+              }
+            });
+          }}
+        />
+        <Button
+          disabled={isDeleting}
+          label="No"
+          variant="basic"
+          onClick={() => {
+            onClose();
+          }}
+        />
+      </div>
+    </>
+  );
+};
